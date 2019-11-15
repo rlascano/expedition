8a263f21b2436ff2b6a44e593a91964400a0198b
Rodrigo <rodrigo.lascano@gmail.com>
2019-11-14 21:40:12 -0300

    Modify migration to pg compatibilty

--- db/migrate/20190628230543_create_tickets.rb
+++ db/migrate/20190628230543_create_tickets.rb
@@ -2,7 +2,7 @@ class CreateTickets < ActiveRecord::Migration[5.2]
   def change
     create_table :tickets do |t|
       t.references :client, foreign_key: true
-      t.integer :carrier, foreign_key: true
+      t.references :carrier, foreign_key: true
       t.references :trucker, foreign_key: true
       t.references :product, foreign_key: true
       t.integer :tare
