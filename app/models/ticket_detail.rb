a16d33bdb3c0b7c318bd883bdd62816eab7ebd18
Rodrigo <rodrigo.lascano@gmail.com>
2019-11-16 18:26:48 -0300

    basic support for truck enter exit time

--- app/models/ticket_detail.rb
+++ app/models/ticket_detail.rb
@@ -2,4 +2,17 @@ class TicketDetail < ApplicationRecord
   belongs_to :product
   belongs_to :category
   belongs_to :ticket
+
+  before_save :save_enter_exit_time
+
+  private
+
+    def save_enter_exit_time
+      if !tare.nil? and enter.nil?
+        self.enter = Time.now
+      end
+      if !gross.nil? and exit.nil?
+        self.exit = Time.now
+      end
+    end
 end
